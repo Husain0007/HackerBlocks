7250e3de8a15dad7d023e60d9f001656d75bb88c
Husain0007 <syedhusainmustafa@gmail.com>
2020-04-14 17:02:54 +0530

    ReplacingPrintFunction

--- 10DaysOfCode/RotateImage/demo.cpp
+++ 10DaysOfCode/RotateImage/demo.cpp
@@ -1,45 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void spiralprint(int a[][100], int m, int n)
+void rotateprint(int **arr, int n, int m)
 {
-    int startRow = 0;
-    int startCol = 0;
-    int endRow = m - 1;
-    int endCol = n - 1;
-    //Print it
-    while (startRow <= endRow && startCol <= endCol)
-    {
-        //First Row
-        for (int i = startCol; i <= endCol; i++)
-        {
-            cout << a[startRow][i] << " ";
-        }
-        startRow++;
-        for (int i = startRow; i <= endRow; i++)
-        {
-            cout << a[i][endCol] << " ";
-        }
-        endCol--;
-        //Bottom Row
-        if (endRow > startRow) // to address rect. matrices
-        {
-            for (int i = endCol; i >= startCol; i--)
-            {
-                cout << a[endRow][i] << " ";
-            }
-            endRow--;
-        }
 
-        //start Col
-        if (endCol > startCol) // for rect. matrices
+    for (int j = m - 1; j >= 0; j--)
+    {
+        for (int i = 0; i < n; i++)
         {
-            for (int i = endRow; i >= startRow; i--)
-            {
-                cout << a[i][startCol] << " ";
-            }
-            startCol++;
+            cout << arr[i][j] << " ";
         }
+        cout << endl;
     }
 }
 
@@ -49,7 +20,12 @@ int main()
     int rows, cols;
     cin >> rows >> cols;
 
-    int array[100][100] = {0};
+    int **array = new int *[rows];
+
+    for (int i = 0; i < rows; i++)
+    {
+        array[i] = new int[cols];
+    }
 
     for (int i = 0; i < rows; i++)
     {
@@ -59,7 +35,7 @@ int main()
         }
     }
 
-    spiralprint(array, rows, cols);
+    rotateprint(array, rows, cols);
 
     return 0;
 }
