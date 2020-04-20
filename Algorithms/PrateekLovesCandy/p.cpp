8bfee7ae4a2f123e0984aa2f21b184bd3432cefa
Husain0007 <syedhusainmustafa@gmail.com>
2020-04-20 18:39:39 +0530

    SieveOfEratosthenes

---
+++ Algorithms/PrateekLovesCandy/p.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#include <vector>
+using namespace std;
+
+// initializing the max value
+#define MAX_SIZE 1000005
+
+void SieveOfEratosthenes(vector<int> &primes)
+{
+
+    bool IsPrime[MAX_SIZE];
+    memset(IsPrime, true, sizeof(IsPrime));
+
+    for (int p = 2; p * p < MAX_SIZE; p++)
+    {
+
+        if (IsPrime[p] == true)
+        {
+            for (int i = p * p; i < MAX_SIZE; i += p)
+                IsPrime[i] = false;
+        }
+    }
+
+    for (int p = 2; p < MAX_SIZE; p++)
+        if (IsPrime[p])
+            primes.push_back(p);
+}
+
+int main()
+{
+    int cases;
+    cin >> cases;
+
+    vector<int> primes;
+    SieveOfEratosthenes(primes);
+
+    while (cases--)
+    {
+
+        int n;
+        cin >> n;
+        cout << primes[n - 1] << endl;
+    }
+    return 0;
+}
