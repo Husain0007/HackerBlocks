2d0559409bbff4774978d97b77e101af9ef31e81
Husain0007 <syedhusainmustafa@gmail.com>
2020-04-21 11:59:27 +0530

    AlgoSet1

---
+++ Algorithms/ManmohamLovesPatterns1/p.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 1 << endl;
+        int i = 2;
+        while (i <= n)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j == 1 || j == i)
+                    {
+                        cout << 1;
+                    }
+                    else
+                    {
+                        cout << 0;
+                    }
+                }
+            }
+            else
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    cout << 1;
+                }
+            }
+            cout << endl;
+            i++;
+        }
+    }
+    return 0;
+}
